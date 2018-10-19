1bc8130ec830976eda7e1e7a0ada6fd2f0105c82
Kevin Huang <yellow.khaos@gmail.com>
2018-10-19 19:34:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 function createPost() {
+
   let postTemplate = document.getElementById('post-template').innerHTML
   let pageTemplate = document.getElementById('page-template').innerHTML
+  let commentTempalte = document.getElementById('comments-template').innerHTML
+
+  let postTitle = document.getElementById('postTitle').value
+  let postBody = document.getElementById('postBody').value
+  let postAuthor = document.getElementById('postAuthor').value
+
+  let mainDiv = document.getElementByTagName("main")
   let postTemplateFn = _.template(postTemplate)
   let pageTemplateFn = _.template(pageTemplate)
+
+  let 
 }
 
 function postComment() {
   let commentTemplate = document.getElementById('comment-template').innerHTML
   let commentTemplateFn = _.template(commentTemplate)
+
 }
+
